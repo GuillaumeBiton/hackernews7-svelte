d7030c233ecb71bdbce799cf363c00b85d37e6c0
GuillaumeBiton <guillaume..biton@free.fr>
2020-05-10 18:06:31 +0200

    unused feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <App params={ f7params }>
-  <a class="skip-link" href="#">Skip to main</a>
 
   <!-- Your main view, should have "view-main" class -->
   <View main class="safe-areas" url="/" masterDetailBreakpoint={800} />
@@ -28,19 +27,5 @@
 <style>
 :global(body) {
     --f7-page-master-width: 33%;
-}
-/* style.css */
-.skip-link {
-  position: absolute;
-  top: -40px;
-  left: 0;
-  background: #000000;
-  color: white;
-  padding: 8px;
-  z-index: 100000;
-}
-
-.skip-link:focus {
-  top: 0;
 }
 </style>
